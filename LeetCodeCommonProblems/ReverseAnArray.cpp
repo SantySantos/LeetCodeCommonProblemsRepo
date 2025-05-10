2b013c66781a8fa7788a1269516deedfee60efb5
SantySantos <santysantos533@gmail.com>
2025-05-09 20:48:12 -0400

    nothing

--- LeetCodeCommonProblems/ReverseAnArray.cpp
+++ LeetCodeCommonProblems/ReverseAnArray.cpp
@@ -6,20 +6,16 @@ using namespace std;
 * Problem: Reverse string
 * Description: Given an array of characters, reverse the array in-place.
 * Link: https://leetcode.com/problems/reverse-string/
-* 
-* 
+*  
 * Approach: two pointer were placed in the array, one at the start, other at the beggining, then the value at the start swaps with th value at the end,
 *			the start pointer increases and the end pointer decreases, this is done until start is equal or greater than end.
 * 
-* Time complexity: N(O)
-*			
+* Time complexity: N(O)			
 */
-
 void BegginerProblems::reverseString(vector<char>& s) {
 
 	int start = 0;
 	int end = s.size() - 1;
-
 
 	while (start < end){
 
