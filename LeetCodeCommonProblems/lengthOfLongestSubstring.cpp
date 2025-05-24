885e618668f7c6c981b1b82d4ffa09a61f32ef5e
SantySantos <santysantos533@gmail.com>
2025-05-24 17:30:39 -0400

    finalizing the longest substring problem using the sliding window technique

--- LeetCodeCommonProblems/lengthOfLongestSubstring.cpp
+++ LeetCodeCommonProblems/lengthOfLongestSubstring.cpp
@@ -1,62 +1,40 @@
 #include <string>
+#include <unordered_map> 
 #include "IntermediateProblems.h"
 
+/*
+* Problem: Longest Substring Without Repeating Characters
+* Description: Given a string s, find the length of the longest substring without duplicate characters.
+* Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+*
+* Approach: using the sliding window technique, we use a pointer to the left and one to the right of the longest substring,
+* and a hasmap to check the chars we have seen and their index. then traversing to the array,we follow by checking if it has
+* been seen before and if their position is greater than the position of the pointer l. if so, we increase the pointer l. if not 
+* it means we can keep moving forward with the substring and update the max length. Finally wwe add the char and the index to the 
+* map to keep track of the character substrings.
+*
+* Time complexity: O(N)
+* 
+* special thanks to Algo Engine : https://www.youtube.com/watch?v=pY2dYa1m2VM for the help finding a solution to this problem and 
+* help me understant the sliding window technique!
+*/
 int IntermediateProblems::lengthOfLongestSubstring(string s) {
-	//anviaj
-	int i = 0;
-	int j = 1;
-	int traversal = i;
-	int size = 0;
-	string longestsSub = "";
-	if (s == "")
-		return 0;
 
-	while (j < s.length()) {
+	int l = 0;
+	unordered_map<char, int> seen;
+	int maxLength = 0;
 
-		if (s[j] != s[j + 1] && s[i] != s[j]) {
-			j++;
-		}
-		else {
-			j++;
-			i++;
-			if (s.size() - i > size)
-				size = s.size() - i;
-		}
+	for (int r = 0; r < s.length(); r++) {
+
+		char ch = s[r];
+
+		if (seen.find(ch) != seen.end() && seen[ch] >= l)
+			l = seen[ch] + 1;
+		else
+			maxLength = max(maxLength, r - l + 1);
+			
+		seen[ch] = r;
 	}
 
-	return j - i + 1;
-
-
-	/*
-	* string longestSubstring = string(1, s[0]);
-	int longestSub = 0;
-	int j = 0;
-
-	for (int i = 1; i <= s.length(); i++) {
-
-		for (j = 0; j <= longestSubstring.length(); j++) {
-
-			if (s[i] == longestSubstring[j]) {
-
-				if (longestSubstring.size() > longestSub) {
-					longestSub = longestSubstring.size();
-				}
-
-				longestSubstring = "";
-
-				if (s[i - 1] != s[i]) {
-					longestSubstring += s[i - 1];
-				}
-
-				break;
-			}
-		}
-
-		j = 0;
-		longestSubstring += s[i];
-	}
-
-	return longestSub;
-	*/
-
+	return maxLength;
 }
