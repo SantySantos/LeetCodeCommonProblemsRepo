e65f292ade2378a03cb8bf60af62a5d3665128b6
SantySantos <santysantos533@gmail.com>
2025-05-30 02:24:42 -0400

    adding description

--- LeetCodeCommonProblems/MinimumSizeSubarraySum.cpp
+++ LeetCodeCommonProblems/MinimumSizeSubarraySum.cpp
@@ -2,6 +2,20 @@
 #include "IntermediateProblems.h"
 #include <vector>
 
+/*
+* Problem: Minimum Size Subarray Sum
+* Description: Given an array of positive integers nums and a positive integer target, 
+*			return the minimal length of a subarray whose sum is greater than or equal to target. 
+*			If there is no such subarray, return 0 instead.
+* Link: https://leetcode.com/problems/minimum-size-subarray-sum/
+*
+* Approach: using the sliding window technique we move the left and right pointer to adjust the currentSum variable and try to 
+get the smallest length that sums up the target. If we traverse all the vector and the currentSum never hitted the target we return 0;
+* 
+*
+* Time complexity: O(N)
+*
+*/
 int IntermediateProblems::minSubArrayLen(int target, vector<int>& nums) {
 
 	int l = 0;
