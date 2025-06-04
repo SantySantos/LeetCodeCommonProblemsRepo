deaff93d05cb3ca569fd0e887b793baa3cb48573
SantySantos <santysantos533@gmail.com>
2025-06-04 00:47:41 -0400

    adding problem description + approach and removing uneccesary comments

--- LeetCodeCommonProblems/RemoveDuplicatesfromSortedArrayII.cpp
+++ LeetCodeCommonProblems/RemoveDuplicatesfromSortedArrayII.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 #include "vector"
 
+/*
+* Problem: Remove Duplicates from Sorted Array II
+* Description: Remove some duplicates in-place such that each unique element appears at most twice.
+* Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+*
+* Approach: using 2 pointers, we check wether l is less than two before checking nums[l -2] as a safe condition, and if nums[r] 
+*			is different than nums[l - 2] it means we can re write the element in nums[l], and so we do, increasing also l. 
+*
+* Time complexity: O(n)
+*/
 int StringsAndArrays::removeDuplicatesII(vector<int>& nums) {
 
 	int l = 0;
@@ -16,57 +26,4 @@ int StringsAndArrays::removeDuplicatesII(vector<int>& nums) {
 	}
 
 	return l;
-
-	/*
-	
-		int l = 0;
-
-for (int r = 0; r < nums.size(); r++) {
-
-	if (l >= 2) {
-		if (nums[l - 2] == nums[l]) {
-
-			if (nums[r] != nums[l])
-			{
-				nums[l] = nums[r];
-			}			
-			else
-				continue;
-		}	
-		else {
-			nums[l] = nums[r];
-		}
-	}
-	
-	l++;
-}
-
-return l;*/
-	/*
-	int l = 0;
-	unordered_map<int, int> counter;
-
-	for (int r = 0; r < nums.size(); r++) {
-
-		counter[nums[l]]++;
-
-		if (counter[nums[l]] > 2) {
-
-			if (nums[l] != nums[r]) {
-
-				int temp = nums[l];
-				nums[l] = nums[r];
-				nums[r] = temp;
-			}
-			else
-				continue;
-		}
-
-		l++;
-
-	}
-	return l;
-	*/
-
-
 }
